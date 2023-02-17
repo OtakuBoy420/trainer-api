d69e3f963ac54b6a10766af3b60b007a7aed383c
OtakuBoy420 <U28282@rts-365.dk>
2023-02-17 12:44:32 +0100

    cors error fix

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 var router = require("./router");
