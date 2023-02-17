b3d72e59d15e549267dd010d76425025ad1bba7d
OtakuBoy420 <U28282@rts-365.dk>
2023-02-17 12:52:18 +0100

    add required auth header

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 var router = require("./router");
