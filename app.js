52157ddae5f8e56b34d4f4aaf09a1d67aba4df29
OtakuBoy420 <U28282@rts-365.dk>
2023-02-17 12:10:34 +0100

    fix cors error

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 require("dotenv").config();
 var express = require("express");
 var app = express();
+app.use(function (req, res, next) {
+  const allowedOrigins = ["http://localhost:3000", "http://trainer-api-oliver.onrender.com", "https://trainer-api-oliver.onrender.com", "https://trainer-api-oliver.onrender.com/"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
 var router = require("./router");
 var { testConnection } = require("./config/database");
 var formidable = require("express-formidable");
 var cors = require("cors");
 
 testConnection();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://trainer-api-oliver.onrender.com", "https://trainer-api-oliver.onrender.com", "https://trainer-api-oliver.onrender.com/"],
+    credentials: true,
+  })
+);
 app.use(formidable());
 app.use(router);
 
